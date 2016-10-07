5c88b209c68d4b750fa96ffe8232493481a60d0f
Zack Stickles <zack.stickles@jumpcloud.com>
2016-10-07 14:41:48 -0600

    Simplified the routes

--- server.js
+++ server.js
@@ -7,19 +7,15 @@ const
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 
-const data = {
-  todos: require('./data/todos')
-};
-
-const controllers = {
-  todos: require('./controllers/todos')(data.todos)
-};
+const
+  data = require('./data/todos')
+  routes = require('./controllers/todos')(data);
 
 // ToDo API routes
-app.get('/api/todos', controllers.todos.getAll);
-app.get('/api/todos/:id', controllers.todos.getOne);
-app.post('/api/todos', controllers.todos.create);
-app.put('/api/todos/:id', controllers.todos.update);
-app.delete('/api/todos/:id', controllers.todos.delete);
+app.get('/api/todos', routes.getAll);
+app.get('/api/todos/:id', routes.getOne);
+app.post('/api/todos', routes.create);
+app.put('/api/todos/:id', routes.update);
+app.delete('/api/todos/:id', routes.delete);
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`) );

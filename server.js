e66b09e2cbce31b7d7b414244ef8cb86c6becfa8
Ryan Douglas <rdouglas@i3logix.com>
2017-09-20 15:24:01 -0600

    updated server to better handle CORS and provide IDs when todo list is empty and a new item is created

--- server.js
+++ server.js
@@ -3,18 +3,29 @@ var
   bodyParser = require('body-parser'),
   app = express(),
   PORT = 3004;
+  
+  app.use(express.static(__dirname + '/public'));
+  app.use(bodyParser.json());
+  
+  app.use(function(req, res, next) {
+    /** If there were a number of environments (Dev, Test, Prod), it would make sense
+     * to have a check for each of those in order for this CORS solution to be more 
+     * roboust. Since we don't, we're just allowing for any port on localhost to be
+     * used.
+    */
+    var localhost = 'http://localhost:';
+    var origin = req.headers.origin;
+    
+    // More robust solution than the PR solution
+    if(origin.lastIndexOf(localhost) > -1){
+      res.setHeader('Access-Control-Allow-Origin', origin);
+    }
+    res.header('Access-Control-Allow-Methods', 'GET, OPTIONS, PUT, POST, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    return next();
+  });
 
-app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
-
-app.use(express.static(__dirname + '/public'));
-app.use(bodyParser.json());
-
-var data = {
+  var data = {
   todos: require('./data/todos')
 };
 
